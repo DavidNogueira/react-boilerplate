fa768064b1099e679f9af29d24f908f4e4d4b8a4
David Nogueira <hsd_sk8@hotmail.com>
2018-05-21 09:50:29 +0100

    commit 2

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 export default class App extends Component {
     render() {
-        return <div>Welcome to David's React Boilerplate App</div>
+        return <div>Welcome to app React David's React Boilerplate App</div>
     }
 }
